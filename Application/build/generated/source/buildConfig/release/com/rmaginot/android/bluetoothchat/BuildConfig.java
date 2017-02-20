f7c1314b8caa761a8855fb1770e18c480d3165c9
Raphaël Maginot <rmaginot@eudoweb.com>
2017-02-20 17:42:43 +0100

    First Commit
    V1

---
+++ Application/build/generated/source/buildConfig/release/com/rmaginot/android/bluetoothchat/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rmaginot.android.bluetoothchat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.rmaginot.android.bluetoothchat";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.0.3";
+}
